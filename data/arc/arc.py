924393852b5604617ce8c5ff74e515fc5d1dcfbb
vinhtran <tranquocvinh2611@gmail.com>
2024-03-31 11:33:44 +0700

    define arc_sample in ds_info

---
+++ data/arc/arc.py
@@ -0,0 +1,33 @@
+from datasets import load_dataset, Dataset
+import pandas as pd
+import json
+
+def convert_multiple_choice_to_prompt(dataset, json_file_path):
+    new_samples = []
+
+    for example in dataset:
+        correct_choice_index = example["choices"]["label"].index(example["answerKey"])
+        correct_choice_text = example["choices"]["text"][correct_choice_index]
+        correct_choice_label = example["choices"]["label"][correct_choice_index]
+
+        for i, (choice_text, choice_label) in enumerate(zip(example["choices"]["text"], example["choices"]["label"])):
+            if i != correct_choice_index:
+                new_samples.append({
+                    "id": example["id"],
+                    "instruction": example["question"],
+                    'input': "",
+                    "output": [correct_choice_text, choice_text],
+                    "choice_label": [correct_choice_label, choice_label],
+                })
+
+
+    # Write to JSON
+    with open(json_file_path, 'w') as json_file:
+        json.dump(new_samples, json_file, indent=4)
+
+if __name__ == "__main__":
+    # Load the original dataset
+    train_dataset = load_dataset("allenai/ai2_arc", "ARC-Easy", split='train[:10]')
+    
+    # Convert multiple choice to prompt, chosen, rejected, and push to hub
+    convert_multiple_choice_to_prompt(train_dataset, 'data/arc/sample.json')
