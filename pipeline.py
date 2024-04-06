f02dfdb6d36e91170631f842add6353b09043bb5
vinhtran <tranquocvinh2611@gmail.com>
2024-04-07 00:55:55 +0700

    remove accelerate

--- pipeline.py
+++ pipeline.py
@@ -236,8 +236,39 @@ def main(args):
 
         active_dataset = new_data_info # replace dataset by ACTIVE QUERIES
         
-        dpo_ft_command = f"""CUDA_VISIBLE_DEVICES={args.gpu_ids} accelerate launch\
-            src/train_bash.py \
+        # dpo_ft_command = f"""CUDA_VISIBLE_DEVICES={args.gpu_ids} accelerate launch\
+        #     src/train_bash.py \
+        #     --stage dpo \
+        #     --do_train \
+        #     --model_name_or_path {args.model_name_or_path} \
+        #     --dataset_dir {args.dataset_dir} \
+        #     --dataset {active_dataset} \
+        #     --template {args.template} \
+        #     --finetuning_type {args.finetuning_type} \
+        #     --lora_target {args.lora_target} \
+        #     --output_dir {dpo_adapter_path} \
+        #     --overwrite_output_dir \
+        #     --cutoff_len {args.cutoff_len} \
+        #     --preprocessing_num_workers 16 \
+        #     --per_device_train_batch_size {args.per_device_train_batch_size} \
+        #     --per_device_eval_batch_size {args.per_device_eval_batch_size} \
+        #     --gradient_accumulation_steps {args.gradient_accumulation_steps} \
+        #     --lr_scheduler_type {args.lr_scheduler_type} \
+        #     --logging_steps {args.logging_steps} \
+        #     --warmup_steps {args.warmup_steps} \
+        #     --save_steps {args.save_steps} \
+        #     --eval_steps {args.eval_steps} \
+        #     --evaluation_strategy {args.evaluation_strategy} \
+        #     --learning_rate {args.learning_rate} \
+        #     --num_train_epochs {args.num_train_epochs} \
+        #     --max_samples {args.max_samples} \
+        #     --val_size {args.val_size} \
+        #     --plot_loss \
+        #     --dpo_ftx 1.0\
+        #     --quantization_bit {args.quantization_bit}
+        #     """
+
+        dpo_ft_command = f"""CUDA_VISIBLE_DEVICES={args.gpu_ids} python src/train_bash.py\
             --stage dpo \
             --do_train \
             --model_name_or_path {args.model_name_or_path} \
@@ -267,6 +298,7 @@ def main(args):
             --dpo_ftx 1.0\
             --quantization_bit {args.quantization_bit}
             """
+        
 
         run_cli_command(dpo_ft_command) 
         print("Done Train DPO")
@@ -276,8 +308,42 @@ def main(args):
         print(" Train Reward ")
         active_dataset = new_data_info # replace dataset by ACTIVE QUERIES
 
-        rm_ft_command = f"""CUDA_VISIBLE_DEVICES={args.gpu_ids} accelerate launch \
-            src/train_bash.py \
+        # rm_ft_command = f"""CUDA_VISIBLE_DEVICES={args.gpu_ids} accelerate launch \
+        #     src/train_bash.py \
+        #     --stage rm \
+        #     --do_train \
+        #     --flash_attn True\
+        #     --model_name_or_path {args.model_name_or_path}\
+        #     --adapter_name_or_path {dpo_adapter_path}\
+        #     --output_dir {reward_model_path} \
+        #     --dataset {active_dataset} \
+        #     --dataset_dir {args.dataset_dir} \
+        #     --template {args.template} \
+        #     --finetuning_type {args.finetuning_type} \
+        #     --lora_target {args.lora_target} \
+        #     --overwrite_cache \
+        #     --overwrite_output_dir \
+        #     --cutoff_len {args.cutoff_len} \
+        #     --preprocessing_num_workers 16 \
+        #     --per_device_train_batch_size {args.per_device_train_batch_size} \
+        #     --per_device_eval_batch_size {args.per_device_eval_batch_size} \
+        #     --gradient_accumulation_steps {args.gradient_accumulation_steps} \
+        #     --lr_scheduler_type {args.lr_scheduler_type} \
+        #     --logging_steps {args.logging_steps} \
+        #     --warmup_steps {args.warmup_steps} \
+        #     --save_steps {args.save_steps} \
+        #     --eval_steps {args.save_steps} \
+        #     --evaluation_strategy {args.evaluation_strategy} \
+        #     --learning_rate {args.learning_rate} \
+        #     --num_train_epochs {args.num_train_epochs} \
+        #     --max_samples {args.max_samples} \
+        #     --val_size {args.val_size} \
+        #     --ddp_timeout 1800000 \
+        #     --plot_loss \
+        #     --quantization_bit {args.quantization_bit}
+        #     """
+
+        rm_ft_command = f"""CUDA_VISIBLE_DEVICES={args.gpu_ids} python src/train_bash.py \
             --stage rm \
             --do_train \
             --flash_attn True\
