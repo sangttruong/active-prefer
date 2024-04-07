89cf7ef572d1620d48efd33ca5eeb95b9c7aa104
vinhtran <tranquocvinh2611@gmail.com>
2024-04-07 15:55:56 +0700

    update predict.sh

--- examples/run_test/predict.sh
+++ examples/run_test/predict.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 
-
-CUDA_VISIBLE_DEVICES=0,1 accelerate launch \
-    --config_file ../accelerate/infer_config.yaml \
-    ../../src/train_bash.py \
-    --stage sft \
-    --do_predict \
+deepspeed --num_gpus 2 ../../src/train_bash.py \
+    --deepspeed ../deepspeed/ds_z3_config.json \
+    --stage rm \
+    --predict \
     --model_name_or_path meta-llama/Llama-2-7b-hf \
-    --dataset arc_challenge\
+    --dataset arc_challenge \
     --dataset_dir ../../data \
     --template default \
-    --finetuning_type lora \
-    --output_dir ../../saves/LLaMA2-7B/lora/predict \
+    --finetuning_type full \
+    --output_dir ../../saves/LLaMA2-7B/full/sft \
     --overwrite_cache \
     --overwrite_output_dir \
     --cutoff_len 1024 \
     --preprocessing_num_workers 16 \
+    --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
-    --max_samples 20 \
-    --predict_with_generate \
-    --quantization_bit 4
-
+    --gradient_accumulation_steps 2 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --warmup_steps 20 \
+    --save_steps 100 \
+    --eval_steps 100 \
+    --evaluation_strategy steps \
+    --learning_rate 5e-5 \
+    --num_train_epochs 3.0 \
+    --max_samples 3000 \
+    --val_size 0.1 \
+    --ddp_timeout 1800000 \
+    --plot_loss 
