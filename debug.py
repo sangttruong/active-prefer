eb9f9bac0590ed01db86f8aa084b9d44646ba1b2
vinhtran <tranquocvinh2611@gmail.com>
2024-04-09 10:56:10 +0700

    fix  debug file

--- debug.py
+++ debug.py
@@ -12,7 +12,7 @@ def read_jsonl(file_path):
 
 def main():
     parser = argparse.ArgumentParser(description='Read JSON Lines file')
-    parser.add_argument('file_path', type=str, help='Path to the JSON Lines file')
+    parser.add_argument('--file_path', type=str, help='Path to the JSON Lines file')
     args = parser.parse_args()
 
     jsonl_data = read_jsonl(args.file_path)
