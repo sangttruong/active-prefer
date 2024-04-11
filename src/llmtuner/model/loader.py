1e83acc4eab04385880c84e4fbeedee66ccb05d1
vinhtran <tranquocvinh2611@gmail.com>
2024-04-11 21:58:59 +0700

    debug training oracle

--- src/llmtuner/model/loader.py
+++ src/llmtuner/model/loader.py
@@ -92,6 +92,9 @@ def load_model(
             logger.warning("Unsloth does not support loading adapters.")
 
     if model is None:
+        print("############################################")
+        print(init_kwargs)
+        print(config)
         model = AutoModelForCausalLM.from_pretrained(model_args.model_name_or_path, config=config, **init_kwargs)
         
     patch_model(model, tokenizer, model_args, is_trainable)
