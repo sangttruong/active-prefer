fdf8d53e35c1ced075e72728a5eb083c4921e778
vinhtran <tranquocvinh2611@gmail.com>
2024-04-16 21:03:47 +0700

    fix path

--- src/llmtuner/train/rm/trainer.py
+++ src/llmtuner/train/rm/trainer.py
@@ -55,7 +55,6 @@ class PairwiseTrainer(Trainer):
         """
         # Compute rewards
         _, _, values = model(**inputs, output_hidden_states=True, return_dict=True)
-        breakpoint()
 
         unwrapped_model: "PreTrainedModel" = self.accelerator.unwrap_model(self.model)
         if getattr(unwrapped_model.config, "model_type", None) == "chatglm":
