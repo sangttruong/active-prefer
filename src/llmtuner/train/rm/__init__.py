3ad0f54f4f4aa9b3f49e12ffe3d5ca50376c21d2
vinhtran <tranquocvinh2611@gmail.com>
2024-04-16 21:15:59 +0700

    fix import

--- src/llmtuner/train/rm/__init__.py
+++ src/llmtuner/train/rm/__init__.py
@@ -1,4 +1,4 @@
 from .workflow import run_rm, run_oracle_rm, run_selection, CustomDataset, ValueHead
 
 
-__all__ = ["run_rm", "run_oracle_rm", "run_selection"]
+__all__ = ["run_rm", "run_oracle_rm", "run_selection", "CustomDataset", "ValueHead"]
