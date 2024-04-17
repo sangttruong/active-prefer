c836f050acf1be19e80d69328f1a49cb844b9043
vinhtran <tranquocvinh2611@gmail.com>
2024-04-18 06:43:30 +0700

    debug train Oracle model

--- pipeline.py
+++ pipeline.py
@@ -236,7 +236,7 @@ def main(args):
     ft_oracle_command = f"""CUDA_VISIBLE_DEVICES={args.gpu_ids} accelerate launch --main_process_port={args.main_process_port}\
         --config_file examples/accelerate/default.yaml \
         src/train_bash.py \
-        --stage rw \
+        --stage rm \
         --do_train \
         --flash_attn True\
         --model_name_or_path {args.model_name_or_path}\
