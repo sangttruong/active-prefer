5a207dcf49b69732a250276f95ffce98f6438234
vinhtran <tranquocvinh2611@gmail.com>
2024-04-18 15:44:21 +0700

    fix num_samples_selected

--- pipeline.py
+++ pipeline.py
@@ -343,7 +343,6 @@ def main(args):
             selection_command = f"""CUDA_VISIBLE_DEVICES={args.gpu_ids} python src/train_bash.py\
                 --stage selection \
                 --do_predict \
-                --active_iter {iter}\
                 --model_name_or_path {args.model_name_or_path} \
                 --dataset_dir {args.dataset_dir} \
                 --dataset {dataset} \
@@ -354,18 +353,12 @@ def main(args):
                 --cutoff_len {args.cutoff_len} \
                 --per_device_train_batch_size {args.per_device_train_batch_size} \
                 --per_device_eval_batch_size {args.per_device_eval_batch_size} \
-                --gradient_accumulation_steps {args.gradient_accumulation_steps} \
-                --lr_scheduler_type {args.lr_scheduler_type} \
                 --logging_steps {args.logging_steps} \
-                --warmup_steps {args.warmup_steps} \
                 --save_steps {args.save_steps} \
-                --eval_steps {args.eval_steps} \
-                --evaluation_strategy {args.evaluation_strategy} \
-                --learning_rate {args.learning_rate} \
-                --num_train_epochs {args.num_train_epochs}\ 
+                --active_iter {iter}\
                 --num_sample_selected {num_sample_selected}
             """
-
+        breakpoint()
         run_cli_command(selection_command) 
 
         ##########################################################
