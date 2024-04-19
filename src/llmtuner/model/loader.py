8e6335b5856b02573f3ca6ec849ed269819f7d91
vinhtran <tranquocvinh2611@gmail.com>
2024-04-19 10:57:01 +0700

    fixed OOM

--- src/llmtuner/model/loader.py
+++ src/llmtuner/model/loader.py
@@ -64,7 +64,6 @@ def load_model(
     r"""
     Loads pretrained model. Must after load_tokenizer.
     """
-    # breakpoint()
     init_kwargs = _get_init_kwargs(model_args)
     config = AutoConfig.from_pretrained(model_args.model_name_or_path, **init_kwargs)
     patch_config(config, tokenizer, model_args, init_kwargs, is_trainable)
