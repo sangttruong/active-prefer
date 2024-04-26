e79aeb3602d7b271d096b7bb5397acc78171614c
vinhtran <tranquocvinh2611@gmail.com>
2024-04-26 10:26:37 +0700

    update single config to eval on multi gpus

--- examples/run_test/reward_multi.sh
+++ examples/run_test/reward_multi.sh
@@ -1,18 +1,80 @@
 #!/bin/bash
 
 
-CUDA_VISIBLE_DEVICES=2,3 accelerate launch --main_process_port=29505\
+CUDA_VISIBLE_DEVICES=3,4,5,6 accelerate launch --main_process_port=29505\
     --config_file examples/accelerate/default.yaml \
     src/train_bash.py \
     --stage rm \
     --do_train \
-    --model_name_or_path meta-llama/Llama-2-7b-hf \
+    --model_name_or_path meta-llama/Llama-2-7b-hf  \
+    --flash_attn True\
+    --dataset reward_bench_train \
+    --dataset_dir data \
+    --template default \
+    --finetuning_type freeze \
+    --output_dir saves/oracle_model_5e-6 \
+    --num_layer_trainable 0\
+    --overwrite_output_dir \
+    --cutoff_len 1024 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --warmup_steps 20 \
+    --save_steps 1000 \
+    --eval_steps 10000 \
+    --evaluation_strategy steps \
+    --learning_rate 5e-6 \
+    --num_train_epochs 20\
+    --ddp_timeout 1800000 \
+    --plot_loss \
+    --report_to none\
+    --fp16
+
+
+CUDA_VISIBLE_DEVICES=5,6,8,9 accelerate launch --main_process_port=29505\
+    --config_file examples/accelerate/default.yaml \
+    src/train_bash.py \
+    --stage rm \
+    --do_train \
+    --model_name_or_path saves/oracle_model \
     --flash_attn False\
     --dataset reward_bench_train \
     --dataset_dir data \
     --template default \
     --finetuning_type freeze \
-    --output_dir saves\
+    --output_dir saves/oracle_model_5e-3 \
+    --num_layer_trainable 0\
+    --overwrite_output_dir \
+    --cutoff_len 1024 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 8 \
+    --lr_scheduler_type cosine \
+    --logging_steps 20 \
+    --warmup_steps 20 \
+    --save_steps 1000 \
+    --eval_steps 10000 \
+    --evaluation_strategy steps \
+    --learning_rate 5e-3 \
+    --num_train_epochs 5\
+    --ddp_timeout 1800000 \
+    --plot_loss \
+    --report_to none\
+    --fp16
+
+------------------------------------------------------
+CUDA_VISIBLE_DEVICES=5 python src/train_bash.py \
+    --stage rm \
+    --do_eval \
+    --model_name_or_path saves/oracle_model_5e-5 \
+    --flash_attn True\
+    --dataset reward_bench_test \
+    --dataset_dir data \
+    --template default \
+    --finetuning_type freeze \
+    --output_dir saves/oracle_model_5e-5\
     --num_layer_trainable 0\
     --overwrite_output_dir \
     --cutoff_len 1024 \
@@ -25,11 +87,13 @@ CUDA_VISIBLE_DEVICES=2,3 accelerate launch --main_process_port=29505\
     --save_steps 1000 \
     --eval_steps 1000 \
     --evaluation_strategy steps \
-    --learning_rate 5e-5 \
-    --num_train_epochs 1 \
-    --max_samples 100 \
-    --val_size 0.1 \
+    --learning_rate 5e-6 \
+    --num_train_epochs 3\
     --ddp_timeout 1800000 \
     --plot_loss \
     --report_to none\
     --fp16
+
+
+
+
