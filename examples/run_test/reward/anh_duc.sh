5a783d354f636444f13b5b23f860c3466225885d
vinhtran <tranquocvinh2611@gmail.com>
2024-04-27 22:07:58 +0700

    refactor folder

--- examples/run_test/reward/anh_duc.sh
+++ examples/run_test/reward/anh_duc.sh
@@ -31,7 +31,7 @@ CUDA_VISIBLE_DEVICES=3,4,5,6 accelerate launch --main_process_port=29505\
 
 
 ----------------------------
-CUDA_VISIBLE_DEVICES=1,2 accelerate launch --main_process_port=29507\
+CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --main_process_port=29507\
   --config_file examples/accelerate/default.yaml \
   src/train_bash.py \
   --stage rm \
