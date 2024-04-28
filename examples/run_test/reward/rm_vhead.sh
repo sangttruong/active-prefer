e4917c5bdd45a7363c67641ab67181f91cde41f3
vinhtran <tranquocvinh2611@gmail.com>
2024-04-28 16:45:09 +0700

    update only store v_head

--- examples/run_test/reward/rm_vhead.sh
+++ examples/run_test/reward/rm_vhead.sh
@@ -1,4 +1,33 @@
 CUDA_VISIBLE_DEVICES=1,2 accelerate launch --main_process_port=29506\
+    --config_file examples/accelerate/single_config.yaml \
+    src/train_bash.py \
+    --stage rm \
+    --do_train \
+    --model_name_or_path meta-llama/Llama-2-7b-hf  \
+    --flash_attn True\
+    --dataset reward_bench_train \
+    --dataset_dir data \
+    --template default \
+    --finetuning_type freeze \
+    --output_dir saves/oracle_model_llama_70b \
+    --num_layer_trainable 0\
+    --overwrite_output_dir \
+    --cutoff_len 1024 \
+    --per_device_train_batch_size 2 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 10 \
+    --warmup_steps 20 \
+    --eval_steps 10000 \
+    --learning_rate 1.5e-4 \
+    --num_train_epochs 1\
+    --max_samples 20\
+    --ddp_timeout 1800000 \
+    --plot_loss \
+    --report_to none\
+    --fp16
+
+CUDA_VISIBLE_DEVICES=1,2 accelerate launch --main_process_port=29506\
     --config_file examples/accelerate/default.yaml \
     src/train_bash.py \
     --stage rm \
