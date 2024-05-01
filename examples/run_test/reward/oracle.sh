75568804cde24d68acf0afa977dc0f25ca691a4e
vinhtran <tranquocvinh2611@gmail.com>
2024-05-01 15:56:40 +0700

    update config

--- examples/run_test/reward/oracle.sh
+++ examples/run_test/reward/oracle.sh
@@ -1,13 +1,17 @@
-CUDA_VISIBLE_DEVICES=3 python src/train_bash.py \
+# Deepspeed
+
+CUDA_VISIBLE_DEVICES=1,3 accelerate launch --main_process_port=29505\
+    --config_file examples/accelerate/default.yaml \
+    src/train_bash.py\
     --stage oracle \
     --do_train \
     --flash_attn True\
-    --model_name_or_path meta-llama/Llama-2-7b-hf \
+    --model_name_or_path meta-llama/Llama-2-70b-hf \
     --dataset_dir data \
     --dataset  reward_bench_train\
     --template default \
     --finetuning_type freeze \
-    --output_dir saves/Llama-2-7b-hf \
+    --output_dir saves/Llama-2-70b-hf \
     --overwrite_output_dir \
     --cutoff_len 1024 \
     --per_device_train_batch_size 2 \
@@ -21,8 +25,40 @@ CUDA_VISIBLE_DEVICES=3 python src/train_bash.py \
     --evaluation_strategy steps \
     --learning_rate 5e-5 \
     --num_train_epochs 1\
-    --is_compute_emb False\
-    --num_oracle 10
+    --is_compute_emb True\
+    --num_oracle 1
+
+
+# ===========================================
+# accelerate
+
+CUDA_VISIBLE_DEVICES=1,3 accelerate launch --main_process_port=29505\
+    --config_file examples/accelerate/single_config.yaml \
+    src/train_bash.py\
+    --stage oracle \
+    --do_train \
+    --flash_attn True\
+    --model_name_or_path meta-llama/Llama-2-70b-hf \
+    --dataset_dir data \
+    --dataset  reward_bench_train\
+    --template default \
+    --finetuning_type freeze \
+    --output_dir saves/Llama-2-70b-hf \
+    --overwrite_output_dir \
+    --cutoff_len 1024 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 4 \
+    --lr_scheduler_type cosine \
+    --logging_steps 400 \
+    --warmup_steps 20 \
+    --save_steps 400 \
+    --eval_steps 4000 \
+    --evaluation_strategy steps \
+    --learning_rate 5e-5 \
+    --num_train_epochs 1\
+    --is_compute_emb True\
+    --num_oracle 1
 
 
 --------------------------------------------
