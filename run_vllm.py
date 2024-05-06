ab8dbb188a5812b8056fee27b63fd30311e00576
vinhtran <tranquocvinh2611@gmail.com>
2024-05-06 15:03:19 +0700

    test inference using off vllm

--- run_vllm.py
+++ run_vllm.py
@@ -1,5 +1,5 @@
-import pandas as pd
 from vllm import LLM, SamplingParams
+import pandas as pd
 
 # Sample prompts.
 prompts = [
@@ -8,31 +8,29 @@ prompts = [
     "The capital of France is",
     "The future of AI is",
 ]
-
 # Create a sampling params object.
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 # Create an LLM.
 model_name_or_path = "meta-llama/Llama-2-7b-hf"
 llm = LLM(model=model_name_or_path)
+# Generate texts from the prompts. The output is a list of RequestOutput objects
+# that contain the prompt, generated text, and other information.
 
-# Initialize a dictionary to store the results.
-results = {"Prompt": []}
+# create df
+results = {"Prompt": prompts}
 
-# Generate texts from the prompts.
 K = 5
-for k in range(1, K + 1):
+for k in range(K):
+    # Infer
+    outputs = llm.generate(prompts, sampling_params)
+    
     results[f"Generated_Text_{k}"] = []
-
-for prompt in prompts:
-    results["Prompt"].append(prompt)
-    outputs = llm.generate([prompt] * K, sampling_params)
-    for k, output in enumerate(outputs, start=1):
+    for output in outputs:
         generated_text = output.outputs[0].text
         results[f"Generated_Text_{k}"].append(generated_text)
 
-# Convert the results dictionary to a DataFrame.
 df = pd.DataFrame(results)
+df.to_csv(f"saves/generated_text_{model_name_or_path}.csv", index=False)
 
-# Display the DataFrame.
-print(df)
+        
