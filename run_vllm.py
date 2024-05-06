c284012cc3c7361350d90d80cf7dec5c837d31a1
vinhtran <tranquocvinh2611@gmail.com>
2024-05-06 15:42:16 +0700

    debug: test inference using off vllm

--- run_vllm.py
+++ run_vllm.py
@@ -2,6 +2,9 @@ import argparse
 import pandas as pd
 import json
 import os
+
+from datasets import Dataset
+
 from vllm import LLM, SamplingParams
 
 def generate_texts(prompts, model_name_or_path, K=5, temperature=0.8, top_p=0.95):
@@ -34,10 +37,10 @@ def get_prompts(dataset, dataset_dir = 'data'):
     with open(dataset_path, 'r') as file:
         data = json.load(file)
     
-    propmts = [x['instruction'] for x in data]
+    propmts = [x['instruction'] for x in data[:10]]
     return propmts
 
-def run_infer(model_name_or_path, dataset, output_dir = 'save'):
+def run_infer(model_name_or_path, dataset, output_dir = 'saves'):
     prompts = get_prompts(dataset)
     df = generate_texts(prompts, model_name_or_path)
 
@@ -48,13 +51,22 @@ def run_infer(model_name_or_path, dataset, output_dir = 'save'):
     csv_path = os.path.join(csv_dir, 'generated_texts.csv')
     df.to_csv(csv_path, index=False)
     print(f"Generated texts saved to {csv_path}")
+
+    # Convert embeddings to dataset
+    train_df = Dataset.from_dict(df)
+
+    # Upload dataset to Hugging Face Hub
+    link = f"vinhtran2611/Generated-texts-{model_name}-{dataset}"
+    train_df.push_to_hub(link, 
+                        commit_message=f"Upload data generated of {dataset}",
+                        token="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")
+    print(f"Uploaded embeddings for {model_name} to Hugging Face Hub.")
     
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Iterative training and evaluation script")
     parser.add_argument("--dataset", type=str, default="reward_bench_train", help="Test")
     parser.add_argument("--model_name_or_path", type=str, default="meta-llama/Llama-2-7b-hf", help="Test")
     parser.add_argument("--K", type=int, default=10, help="num iteration")
-
     return parser.parse_args()
 
 def main():
