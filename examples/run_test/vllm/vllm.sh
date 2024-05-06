63a88489887647286283b3807b0da07861b9b0d5
vinhtran <tranquocvinh2611@gmail.com>
2024-05-06 14:52:43 +0700

    test inference using off vllm

--- examples/run_test/vllm/vllm.sh
+++ examples/run_test/vllm/vllm.sh
@@ -1,8 +1,8 @@
 # vinhtran2611/test_export_model
 
-CUDA_VISIBLE_DEVICES=0,1 API_PORT=8005 python src/api_demo.py \
+CUDA_VISIBLE_DEVICES=6 API_PORT=8005 python src/api_demo.py \
     --model_name_or_path meta-llama/Llama-2-7b-hf \
-    --template llama2 \
+    --template default \
     --infer_backend vllm \
     --vllm_enforce_eager
 
